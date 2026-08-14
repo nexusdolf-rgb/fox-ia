c2bce9db0a72207f5825a903c9a14fdb89ea1415
nexusdolf-rgb <nexusdolf@gmail.com>
2026-08-14 22:23:50 +0200

    start.js

---
+++ start.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const candidats = fs.readdirSync(__dirname)
+  .filter(f => /^index.*\.js$/i.test(f) && f !== 'start.js')
+  .map(f => {
+    const st = fs.statSync(path.join(__dirname, f));
+    return { f, size: st.size, time: st.mtimeMs };
+  })
+  .filter(c => c.size > 50000)
+  .sort((a, b) => (b.size - a.size) || (b.time - a.time));
+
+if (!candidats.length) {
+  console.error('Fichier principal introuvable.');
+  process.exit(1);
+}
+
+console.log('Demarrage de Fox IA depuis : ' + candidats[0].f);
+require(path.join(__dirname, candidats[0].f));
